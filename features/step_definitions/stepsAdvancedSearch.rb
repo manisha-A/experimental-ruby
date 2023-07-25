80e1971d9d1fba6de4da5e74ef59f02a6efd6b61
Manisha Awasthi <manisha@192.168.1.136>
2023-07-25 19:46:06 +0100

    use github_workspace

--- features/step_definitions/stepsAdvancedSearch.rb
+++ features/step_definitions/stepsAdvancedSearch.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'rspec'
 require 'selenium-webdriver'
 
-Selenium::WebDriver::Chrome::Service.driver_path = "./features/support/driver/chromedriver"
+# Selenium::WebDriver::Chrome::Service.driver_path = "./features/support/driver/chromedriver"
+Selenium::WebDriver::Chrome::Service.driver_path = ENV['CHROMEDRIVER']
+printf(ENV['CHROMEDRIVER'])
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless=new')
 options.add_argument('--disable-gpu')
