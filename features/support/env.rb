b2f09e2ad7e85b434e6315d2da0b8cb6f197ef0f
Manisha Awasthi <manisha@192.168.1.136>
2023-07-25 20:55:34 +0100

    pass chromedriver path

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
 if ENV['CI'] = 'true'
-  export CHROMEDRIVER="./features/support/driver/chromedriver_mac/chromedriver"
+  CHROMEDRIVER="./features/support/driver/chromedriver_mac/chromedriver"
 else
-  export CHROMEDRIVER="./features/support/driver/chromedriver_mac/chromedriver"
+  CHROMEDRIVER="./features/support/driver/chromedriver_mac/chromedriver"
 end
